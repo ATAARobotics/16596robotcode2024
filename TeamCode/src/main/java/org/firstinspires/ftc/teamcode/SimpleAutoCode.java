3fffe305917192fe1fce05d7a9234e46388132cb
Swatbots-Red <56233077+Swatbots-Red@users.noreply.github.com>
2023-10-30 21:31:58 -0600

    start simiple auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleAutoCode.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class SimpleAutoCode { private final ElapsedTime runtime = new ElapsedTime();
+    private Motor leftFrontDrive = null;
+    private Motor rightFrontDrive = null;
+    private Motor leftBackDrive = null;
+    private Motor rightBackDrive = null;
+
+    private Motor arm1 = null;
+    public void runOpMode()
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/readme.md
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/readme.md
@@ -3,4 +3,6 @@ This is start of README for team 16595, 2024 Center Stage.
 to enable wifi direct in AS, need to navigate to C:\Users\bruce\AppData\Local\Android\Sdk\platform-tools
 then type  .\adb connect 192.168.43.1:5555
 
+
+
 =======================================================
