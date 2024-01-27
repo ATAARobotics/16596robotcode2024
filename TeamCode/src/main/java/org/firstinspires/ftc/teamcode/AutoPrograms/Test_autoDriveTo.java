e73bf8ae2b9fc80219fc4cd096f40963f1d90c40
ftc16596 <ftc16596@ataarobotics.ca>
2024-01-27 12:08:54 -0700

    comp2: wrist action/control modified

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPrograms/Test_autoDriveTo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPrograms/Test_autoDriveTo.java
@@ -1,16 +1,54 @@
-//package org.firstinspires.ftc.teamcode.AutoPrograms;
-//
-//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-//
-//@Autonomous(name = "TestAutoDriveTo",group = "")
-//public class Test_autoDriveTo extends AutoOpMode{
-//
-//    public void runOpMode() {
+package org.firstinspires.ftc.teamcode.AutoPrograms;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.JustTesting.TestAutoOpMode;
+import org.firstinspires.ftc.teamcode.mechanisms.TestDriveTrain;
+
+@Autonomous(name = "TestAutoDriveTo",group = "")
+public class Test_autoDriveTo extends TestAutoOpMode {
+    int step = 0;
+    public void loop() {
+        super.loop();
+        driveTrain.printTelemetry(telemetry);
+        telemetry.update();
 //        int zone = cam.detectElement();
 //        telemetry.addData("zone =:",zone);
 //        telemetry.update();
-//// After "start" do this - test driveTo:
-//        driveTrain.driveTo(.5,20,20);
-//
-//    }//end of runOpMode
-//}//end of class
+// After "start" do this - test driveTo:
+        switch (step) {
+            case 0: // Move Forward
+                driveTrain.resetOdometry();
+                driveTrain.driveTo(1, 100, 0);
+                step++;
+                break;
+            case 1:
+                if (driveTrain.atTarget()) {
+                    step++;
+                    driveTrain.stop();
+                }
+                break;
+//            case 2: // Move Forward
+//                driveTrain.setDirection(driveTrain.left);
+//                step++;
+//                break;
+//            case 3:
+//                if (driveTrain.onHeading()) {
+//                    step++;
+//                    driveTrain.stop();
+//                }
+//                break;
+//            case 4: // Move Forward
+//                driveTrain.resetOdometry();
+//                driveTrain.driveTo(.5, -20, 0);
+//                step++;
+//                break;
+//            case 5:
+//                if (driveTrain.atTarget()) {
+//                    step++;
+//                    driveTrain.stop();
+//                }
+//                break;
+        }
+    }
+}//end of class
