192a1ddd96b9bd93f99b2196c4b10505c7398654
ftc16596 <ftc16596@ataarobotics.ca>
2024-02-03 12:30:21 -0700

    Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/DriveTrain.java
@@ -73,8 +73,8 @@ public class DriveTrain {
         driveBase = new MecanumDrive(leftFrontDrive,rightFrontDrive,leftBackDrive,rightBackDrive);
     }
     public void init() {
-        headingControl = new PIDController(0.03, 0.2, 0.0005 );
-        headingControl.setTolerance(1);
+        headingControl = new PIDController(0.01, 0.002, 0.0005 );
+        headingControl.setTolerance(10);// was 1 ..cbw
         xControl = new PIDController(0.07, 0.2, 0.01);
         yControl = new PIDController(0.07, 0.2, 0.01);
 
@@ -107,7 +107,7 @@ public class DriveTrain {
             }
         }
 
-        // PID controller
+        // PID controller for heading
         headingControl.setSetPoint(headingSetPoint);
         headingCorrection = -headingControl.calculate(heading);
 
